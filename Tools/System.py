aa9d1244e096b68bcf391cd414a03c2f418abd40
vadimklimenko86 <107954238+vadimklimenko86@users.noreply.github.com>
2025-07-09 17:37:14 +0300

    Add newline at file ends

--- Tools/System.py
+++ Tools/System.py
@@ -2,20 +2,16 @@ from MCP_Tools import MCP_Tools
 import mcp.types as types
 import datetime
 
+
 class SystemTools:
-	def __init__(self, mcp: MCP_Tools) -> None:
-		tz_plus3 = datetime.timezone(datetime.timedelta(hours=3))
+    def __init__(self, mcp: MCP_Tools) -> None:
+        tz_plus3 = datetime.timezone(datetime.timedelta(hours=3))
 
-
-
-		@mcp.register_tool(name="gettime", description="Получить текущее время")
-		def gettime()->list[types.TextContent
-		| types.ImageContent
-		| types.EmbeddedResource]:
-			return [
-				types.TextContent(
-								type="text",
-								text=f"Current time: {datetime.datetime.now(tz_plus3).isoformat()}"
-						)
-			]
-		
+        @mcp.register_tool(name="gettime", description="Получить текущее время")
+        def gettime() -> list[types.TextContent | types.ImageContent | types.EmbeddedResource]:
+            return [
+                types.TextContent(
+                    type="text",
+                    text=f"Current time: {datetime.datetime.now(tz_plus3).isoformat()}"
+                )
+            ]
